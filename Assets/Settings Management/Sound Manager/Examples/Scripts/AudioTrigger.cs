02824524a65057e4c500072bfd9ba63ce733931c
Sean Lange <sean@webthingee.com>
2019-02-04 14:22:29 -0800

    sound added

---
+++ Assets/Settings Management/Sound Manager/Examples/Scripts/AudioTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioEvent audioEvent;
+    public bool interuptable;
+	
+    private AudioSource audioSource;
+
+	private void OnMouseDown()
+	{
+		audioSource = SoundManager.instance.GetOpenAudioSource();
+        audioEvent.Play(audioSource);
+	}	
+    
+    public void OnMouseUp ()
+	{
+        if (interuptable)
+            audioSource.Stop();
+	}
+}
